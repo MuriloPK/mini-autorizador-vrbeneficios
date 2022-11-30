952c309a53d0354539a3a408386da387de4419fa
MuriloPK <murilosouzadecarvalho@gmail.com>
2022-11-30 11:51:51 -0300

    inicia projeto

---
+++ src/main/java/br/com/vrbeneficios/miniautorizadorvrbeneficios/MiniAutorizadorVrbeneficiosApplication.java
@@ -0,0 +1,13 @@
+package br.com.vrbeneficios.miniautorizadorvrbeneficios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniAutorizadorVrbeneficiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniAutorizadorVrbeneficiosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vrbeneficios/miniautorizadorvrbeneficios/MiniAutorizadorVrbeneficiosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vrbeneficios.miniautorizadorvrbeneficios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniAutorizadorVrbeneficiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
